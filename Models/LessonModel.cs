5fe113dc1d43c7ce217f7ad86397e1f23b058d4d
Modestas <modestask97@gmail.com>
2020-03-03 13:41:36 +0000

    seperated lesson and module in seperate files

--- Models/LessonModel.cs
+++ Models/LessonModel.cs
@@ -1,17 +1,11 @@
 using System;
 namespace wealthKernelApp.Models
 {
-  public class Module
-  {
-    public string Title { get; set; }
-    public string Copy { get; set; }
-  }
-
-  public class Lesson
+  public class LessonModel
   {
     public string Title { get; set; }
     public string Slug { get; set; }
-    public Module[] Modules { get; set; }
+    public wealthKernelApp.Models.ModuleModel[] Modules { get; set; }
   }
 
 }
---
+++ Models/ModuleModel.cs
@@ -0,0 +1,10 @@
+using System;
+namespace wealthKernelApp.Models
+{
+  public class ModuleModel
+  {
+    public string Title { get; set; }
+    public string Copy { get; set; }
+  }
+
+}
