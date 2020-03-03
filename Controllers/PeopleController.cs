a0921ddfb5faba5e8d6d83b386cd108132720520
Modestas <modestask97@gmail.com>
2020-03-03 11:48:05 +0000

    test list people view using person model

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using wealthKernelApp.Models;
@@ -7,21 +8,26 @@ namespace wealthKernelApp.Controllers
   public class PeopleController : Controller
   {
     // GET: people
-    public IActionResult Index()
+    public ActionResult Index()
     {
       return View();
     }
 
-    public IActionResult ListPeople()
+    public ActionResult ListPeople()
     {
-
+      // create a list of people 
       List<PersonModel> people = new List<PersonModel>();
 
+      // add new people to list
       people.Add(new PersonModel { FirstName = "Tim", LastName = "Timson", Age = 10 });
       people.Add(new PersonModel { FirstName = "Jen", LastName = "Jenson", Age = 19 });
       people.Add(new PersonModel { FirstName = "John", LastName = "Jonhson", Age = 30 });
 
-      return View();
+      Console.WriteLine("PEOPLE ", people);
+
+      // pass list of people data to the people view
+      // generally you would only pass in a model or list of model as data to the view
+      return View(people);
     }
 
   }
