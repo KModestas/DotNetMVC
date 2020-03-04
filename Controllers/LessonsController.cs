316b901a5fc795d7b72a24230b15c108aba1f984
Modestas <modestask97@gmail.com>
2020-03-04 12:04:21 +0000

    passing data from view to controller

--- Controllers/LessonsController.cs
+++ Controllers/LessonsController.cs
@@ -15,12 +15,17 @@ namespace wealthKernelApp.Controllers
       var httpClient = new System.Net.Http.HttpClient();
       var client = new ContentfulClient(httpClient, "TJUTn86tuAZZ0L4Aw1LmA2HUBWNoU2TBHeCghBWJTac", "XsWxHVfA6Mu5yO2rlMf_IlrmIxfy5nAnFPraPg5LGc0", "dgv1c069l5im");
 
-
-      var contentType = await client.GetContentType("lesson");
-
       var entries = await client.GetEntriesByType<Lesson>("lesson");
 
       return View(entries);
+    }
+
+    public ActionResult Lesson(string slug)
+    {
+
+      ViewData["slug"] = slug;
+
+      return View();
     }
 
   }
