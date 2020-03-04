95d4d9d0e91eb269459500f3a2a1b14abecdf401
Modestas <modestask97@gmail.com>
2020-03-04 13:57:01 +0000

    single lessons page

--- Controllers/LessonsController.cs
+++ Controllers/LessonsController.cs
@@ -17,15 +17,27 @@ namespace wealthKernelApp.Controllers
 
       var entries = await client.GetEntriesByType<Lesson>("lesson");
 
+      // foreach (var row in entries) Console.WriteLine(row.Slug);
+
       return View(entries);
     }
 
-    public ActionResult Lesson(string slug)
+    public async Task<ActionResult> Lesson(string slug)
     {
 
+      // slug is being passed in by the actionLink in /Lessons
       ViewData["slug"] = slug;
 
-      return View();
+      var httpClient = new System.Net.Http.HttpClient();
+      var client = new ContentfulClient(httpClient, "TJUTn86tuAZZ0L4Aw1LmA2HUBWNoU2TBHeCghBWJTac", "XsWxHVfA6Mu5yO2rlMf_IlrmIxfy5nAnFPraPg5LGc0", "dgv1c069l5im");
+
+      // if you had the entry id you could just return the single entry without having to do a forEach in the view
+      var entry = await client.GetEntries<Lesson>($"?content_type=lesson&fields.slug={slug}");
+
+      // logs the total number of entries returned
+      // Console.WriteLine(entries.Total.ToString());
+
+      return View(entry);
     }
 
   }
